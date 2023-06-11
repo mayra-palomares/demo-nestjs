6076eca8d21e87cd39ede89d71ce33f874e8b244
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-06-11 22:51:21 +0100

    Create new Auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+    login(){
+        return {msg: 'I am logged in'}
+    }
+    
+    signup(){
+        return {msg: 'I am signed up'}
+    }
+}
