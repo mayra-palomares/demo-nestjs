93d337eef6a0a82cf3477983a7d081300e92a930
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-08-16 14:03:20 +0100

    Add Passport guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UserController {
 
     constructor(private userService: UserService) { }
 
+    @UseGuards(AuthGuard('jwt'))
     @Get("me")
     getAuthenticatedUser() {
         return this.userService.getAuthenticatedUser();
