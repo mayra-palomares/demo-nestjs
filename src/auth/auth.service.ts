1f199d0f9c8228b7608465e6d68927daab71933a
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-08-16 12:00:46 +0100

    Generate access token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { LoginDto, SignUpDto } from "./dto";
 import * as argon from "argon2";
 import { User } from "@prisma/client";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 @Injectable({})
 export class AuthService {
 
-    constructor(private prisma: PrismaService) { }
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) { }
 
     async login(dto: LoginDto) {
         const user: User = await this.prisma.user.findFirst({
@@ -24,10 +26,7 @@ export class AuthService {
         if (!passwordMatches) {
             throw new ForbiddenException('Invalid password');
         }
-
-        delete user.password;
-
-        return user;
+        return this.signToken(user.id, user.email);
     }
 
     async signup(dto: SignUpDto) {
@@ -43,14 +42,24 @@ export class AuthService {
                 }
             })
 
-            delete user.password;
-            return user;
+            return this.signToken(user.id, user.email);
         } catch (error) {
             if (error.code === 'P2002') {
                 throw new ForbiddenException('The email is already in use.');
             }
 
             throw error;
+        }
+    }
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        }
+        const token = await this.jwt.signAsync(payload, { expiresIn: '15m', secret: this.config.get('JWT_SECRET') })
+        return {
+            access_token: token
         }
     }
 }
